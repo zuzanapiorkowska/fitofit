a8134c9292530458378ef9793dfd6c56234a96bb
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-04-12 11:16:39 +0200

    add interfaces

---
+++ interfaces/Workout.ts
@@ -0,0 +1,50 @@
+type Discipline = "biking" | "swimming" | "running";
+type GoalType = "distance" | "workoutCount";
+type Gender = "male" | "female" | "other" | "prefer not to say";
+
+export interface IPartialWorkout {
+  discipline: Discipline;
+  distanceInMeters: number;
+  durationInSeconds: number;
+}
+
+export interface IWorkout {
+  userId: string;
+  workoutId: string;
+  date: string;
+  parts: IPartialWorkout[];
+  notes?: string;
+}
+
+export interface IUserLogin {
+  userId: string;
+  username: string;
+  password: string;
+}
+
+export interface IUserDetails {
+  userId: string;
+  username: string;
+  gender?: Gender;
+  about?: string;
+  avatar?: string;
+  goals?: IGoal[];
+  workouts?: IWorkout[];
+}
+
+export interface IGoal {
+  userId: string;
+  goalId: string;
+  type: GoalType;
+  startDate: string;
+  endDate: string;
+  isCompleted: boolean;
+  goal: number;
+  note?: string;
+}
+
+export interface IStandardResponse {
+  statusCode: number;
+  message: string;
+  error?: string;
+} 
